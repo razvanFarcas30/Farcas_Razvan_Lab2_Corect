87315d7d31008c3f8a40531f996f04dd4ecd77ed
razvanFarcas30 <razvan.farcas2002@gmail.com>
2023-11-12 20:34:05 +0200

    lab5_ok

---
+++ Areas/Identity/Data/LibraryIdentity2Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Farcas_Razvan_Lab2_incercareaNR2.Data;
+
+public class LibraryIdentity2Context : IdentityDbContext<IdentityUser>
+{
+    public LibraryIdentity2Context(DbContextOptions<LibraryIdentity2Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
